2a4fd58f3bd46c1b0e3673346fdce4e0e9a9a519
Sora Arakawa <mail@arkw.net>
2023-03-15 20:05:47 +0900

    Post current time

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 # shishi-zekki-alert
 # (c) 2023 Sora Arakawa all rights reserved.
 
-import os, json
+import os, json, datetime
 from dotenv import load_dotenv
 from requests_oauthlib import OAuth1Session
 
@@ -12,7 +12,6 @@ CONSUMER_SECRET = os.getenv("CONSUMER_SECRET")
 TOKEN = os.getenv("TOKEN")
 TOKEN_SECRET = os.getenv("TOKEN_SECRET")
 twitter = OAuth1Session(CONSUMER_KEY, CONSUMER_SECRET, TOKEN, TOKEN_SECRET)
-url = f"https://api.twitter.com/2/users/{TWITTER_ID}/tweets"
 params = {
     "expansions": "author_id",
     "tweet.fields": "created_at,public_metrics",
@@ -20,9 +19,12 @@ params = {
     "max_results": 10,
 }
 
-res = twitter.get(url, params = params)
+res = twitter.get(f"https://api.twitter.com/2/users/{TWITTER_ID}/tweets", params = params)
 if res.status_code == 200:
     tl = json.loads(res.text)
     for i in range(len(tl["data"])):
         if tl["data"][i]["text"].find(os.getenv("TWITTER_KEYWORD")) >= 0:
             print(tl["data"][i]["id"])
+    dt_now = datetime.datetime.now()
+    payload = {"text": f"{dt_now.strftime('%Y/%m/%d %H:%M')} 投稿"}
+    response = twitter.post("https://api.twitter.com/2/tweets", json = payload)
