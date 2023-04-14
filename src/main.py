c15236c239113ea2226143fac26fb8b43514480f
Sora Arakawa <mail@arkw.net>
2023-04-14 22:14:56 +0900

    Reduce frequency of automated tweet

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 from requests_oauthlib import OAuth1Session
 
 APP_NAME = "shishi-zekki-alert"
-APP_VERSION = "1.1.1"
+APP_VERSION = "1.2"
 APP_COPYRIGHT = "(c) 2023 Sora Arakawa all rights reserved."
 SAVE_FILE_PATH = "/src/savedata"
 
@@ -35,23 +35,27 @@ def main():
     print(APP_COPYRIGHT)
     print("----------")
     while True:
+        tweet_flag = False
         dt_now = datetime.datetime.now()
         print("Twitter timeline check: " + dt_now.strftime('%Y/%m/%d %H:%M'))
         res = twitter.get(f"https://api.twitter.com/2/users/{TWITTER_ID}/tweets", params = params)
         if res.status_code == 200:
             tl = json.loads(res.text)
             for i in range(len(tl["data"])):
-                if tl["data"][i]["text"].find(os.getenv("TWITTER_KEYWORD")) >= 0 and (tl["data"][i]["id"] in tweet_id) == False:
-                    payload = {"text": f"{dt_now.strftime('%Y/%m/%d %H:%M')} {TWEET_TEXT}\n{TWEET_HASHTAG}"}
-                    response = twitter.post("https://api.twitter.com/2/tweets", json = payload)
-                    print("Automatically tweet: " + tl["data"][i]["id"])
-                    f = open(SAVE_FILE_PATH, "w")
-                    f.write(tl["data"][i]["id"])
-                    f.close()
-                    tweet_id.append(tl["data"][i]["id"])
-                    if len(tweet_id) > 20:
-                        tweet_id.pop(0)
-                    break
+                if tl["data"][i]["text"][0:3] != "RT " and tl["data"][i]["text"][0:1] != "@":
+                    if tl["data"][i]["text"].find(os.getenv("TWITTER_KEYWORD")) >= 0 and (tl["data"][i]["id"] in tweet_id) == False:
+                        print("Found post matching keyword: " + tl["data"][i]["id"])
+                        if tweet_flag == False:
+                            payload = {"text": f"{dt_now.strftime('%Y/%m/%d %H:%M')} {TWEET_TEXT}\n{TWEET_HASHTAG}"}
+                            response = twitter.post("https://api.twitter.com/2/tweets", json = payload)
+                            print("Automatically tweet: " + tl["data"][i]["id"])
+                            tweet_flag = True
+                        f = open(SAVE_FILE_PATH, "w")
+                        f.write(tl["data"][i]["id"])
+                        f.close()
+                        tweet_id.append(tl["data"][i]["id"])
+                        if len(tweet_id) > 20:
+                            tweet_id.pop(0)
         time.sleep(60)
 
 if __name__ == "__main__":
